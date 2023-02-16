3e3856d5c5626bf7c53a08bd9c37f4de049f258a
Vishnu <vishnukp2529@gmail.com>
2023-02-16 16:40:23 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ProductDetails from "./Components/Pages/ProductDetails";
+
+function App() {
+  return (
+    <div className="App">
+      <ProductDetails/>
+    </div>
+  );
+}
+
+export default App;
